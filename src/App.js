ea6a9edd12e8f6111a272b37c5890d5393a5d708
MikkoKarvonen <mkarvonen34@gmail.com>
2019-02-14 19:25:16 +0200

    Add dummy data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+let dummy = {
+  "courses": [
+    {
+      "title_fi": "Jauhelihakeittoa",
+      "category": "Lounas",
+      "properties": "G, M",
+      "desc_fi": "",
+      "desc_en": "",
+      "desc_se": ""
+    },
+    {
+      "title_fi": "Mustajuurisosekeittoa",
+      "category": "Kasvislounas",
+      "properties": "G, L",
+      "desc_fi": "",
+      "desc_en": "",
+      "desc_se": ""
+    },
+    {
+      "title_fi": "Mustajuurisosekeittoa, broileria ja fetasalaattia",
+      "category": "Deli Salad Garden",
+      "properties": "G, L",
+      "desc_fi": "",
+      "desc_en": "",
+      "desc_se": ""
+    },
+    {
+      "title_fi": "Yst\u00e4v\u00e4np\u00e4iv\u00e4leivosta",
+      "category": "J\u00e4lkiruoka",
+      "properties": "L",
+      "desc_fi": "",
+      "desc_en": "",
+      "desc_se": ""
+    }
+  ]
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h1>Pitskun lounas</h1>
+        {dummy.courses.map((item,i) => <li key={i}>{item.title_fi}</li>)}
       </div>
     );
   }
